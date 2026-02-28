8140f3bf86e9bd6fb17c7c87fe89170f15300f5a
Amitesh Kumar <kumay047@mymail.unisa.edu.au>
2026-02-28 13:23:17 +1030

    Added data ingestion script for raw csv files obtained from OpenElectricity. Removing characters not supported in MySQL column names or which made refrencing the column names difficult.

---
+++ src/ingest_data.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+
+
+df = pd.read_csv("../data/SouthAustralia_generation.csv")
+
+# Clean csv file up before loading
+df.columns = df.columns.str.strip().str.lower().str.replace(" ", "_").str.replace(r"[^a-z0-9_]", "", regex=True).str.replace("__", "_")
+df["date"] = pd.to_datetime(df["date"])
+
+load_dotenv()
+mysql_password = os.getenv("MYSQL_PASSWORD")
+engine = create_engine(f"mysql+pymysql://root:{mysql_password}@localhost/sapn_grid")
+df.to_sql("nem_all", con=engine, if_exists="replace", index=False)
